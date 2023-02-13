ae842ad235cf5305a3dad4b3b76265628f2437e1
chenzuoli <chenzuoli709@163.com>
2023-02-13 23:49:40 +0800

    init commit

---
+++ api/index.js
@@ -0,0 +1,81 @@
+const { Configuration, OpenAIApi } = require("openai");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const operandClient = require("@operandinc/sdk").operandClient;
+const indexIDHeaderKey = require("@operandinc/sdk").indexIDHeaderKey;
+const ObjectService = require("@operandinc/sdk").ObjectService;
+
+// Open AI Configuration
+const configuration = new Configuration({
+  organization: process.env.OPENAI_ORG_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// Express Configuration
+const app = express();
+const port = 3080;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(require("morgan")("dev"));
+
+// Routing
+
+// Primary Open AI Route
+app.post("/", async (req, res) => {
+  const { message } = req.body;
+
+  const runIndex = async () => {
+    const operand = operandClient(
+      ObjectService,
+      // process.env.OPERAND_KEY,
+	  "bdxgbb5ob9tchx3ym7ujs9c7oex99mok8ivr",
+      "https://api.operand.ai",
+      {
+        // [indexIDHeaderKey]: process.env.OPERAND_INDEX_KEY,
+		[indexIDHeaderKey]: "hkd9o780woe8"
+      }
+    );
+
+    try {
+      const results = await operand.searchWithin({
+        query: `${message}`,
+        limit: 5,
+      });
+
+      if (results) {
+        return results.matches.map((m) => `- ${m.content}`).join("\n");
+      } else {
+        return "";
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  let operandSearch = await runIndex(message);
+
+  const basePromptPrefix = `This is a conversation between bloger Chenzuoli and a stranger.\nRelevant information that zuoli knows:\n${operandSearch}`;
+
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${basePromptPrefix}\n\nStranger:${message}\n\nChenzuoli:`,
+    max_tokens: 256,
+    temperature: 0.7,
+  });
+  res.json({
+    message: response.data.choices[0].text,
+  });
+});
+
+// Get Models Route
+
+// Start the server
+app.listen(port, () => {
+  console.log(`server running`);
+});
+
+module.exports = app;
